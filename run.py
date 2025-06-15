d5902d58571071ce28daf68f7f3a0728681a60da
Khaled <khaledshbrawy@gmail.com>
2025-06-15 20:29:27 +0300

    012021202yarab

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 # run.py
 from app import create_app
 from dotenv import load_dotenv; load_dotenv()
+from flask_cors import CORS
 
 app = create_app()
+
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
